2f0ef10de2ea6786d25836ba638e0c4698b3470c
Cee Jay <ceejaymalacas17@gmail.com>
2025-04-20 20:04:06 +0800

    icons added/designed scrollbar

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="container w-full border-4 border-gray-300 flex gap-4 bg-gray-300">
-        <div className="bg-gray-200 w-8/12 h-6 border-l-2 border-t-2 border-r-2 border-b-2 border-gray-400 border-b-gray-200"></div>
-        <div className="bg-gray-200 w-4/12 h-6 border-l-2 border-t-2 border-r-2 border-b-2 border-gray-400 border-b-gray-200"></div>
+        <div className="bg-gray-300 w-8/12 h-6 border-l-2 border-t-2 border-r-2 border-b-2 border-gray-400 border-b-gray-200 border-r-gray-200"></div>
+        <div className="bg-gray-300 w-4/12 h-6 border-l-2 border-t-2 border-r-2 border-b-2 border-gray-400 border-b-gray-200 border-r-gray-200"></div>
     </div>
   )
 }
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <div className="container bg-white w-full border-l-4 border-r-4 border-b-4 border-gray-300">
-      <div className="p-2 border-l-2 border-t-2 border-l-gray-500 border-t-gray-500 border-b-2 border-b-gray-200 border-r-2 border-r-gray-200">
+      <div className="p-2 border-l-2 border-t-2 border-l-gray-500 border-t-gray-500 border-b-2 border-b-gray-200 border-r-2 border-r-gray-200 h-[500px] overflow-y-scroll win95-scroll">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro at eius
         molestiae vel excepturi inventore autem eum pariatur. Voluptate quidem
         id eligendi reiciendis, ab laboriosam quod? Est quasi tempora repellat?
